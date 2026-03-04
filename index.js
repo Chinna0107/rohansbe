e6b11ff5c81e6bda6a3d9bbcfacb7f9642856fb1
chinna0107 <kancharlahemanth89@gmail.com>
2026-03-04 19:04:52 +0530

    Production 5.0

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const sliderRoutes = require('./routes/sliderRoutes');
-// const app = express();
-// const PORT = 3000;
-
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
@@ -23,7 +20,6 @@ app.use(
     credentials: true,
   })
 );
-app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
